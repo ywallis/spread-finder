5c2b3564699460b08c77c440a592c9f5bbb03fec
Yann <yann.wallis@gmail.com>
2024-10-28 07:20:56 +0800

    Included try-except in case ticker unexpectedly returns a none.

--- calculate_spread.py
+++ calculate_spread.py
@@ -16,29 +16,32 @@ def calculate_spread(hl_exchange, ll_exchange, joint_tickers, spread_threshold):
 
     for ticker in joint_tickers:
 
-        hl_ask = hl_tickers[ticker]['ask']
-        hl_bid = hl_tickers[ticker]['bid']
-        ll_ask = ll_tickers[ticker]['ask']
-        ll_bid = ll_tickers[ticker]['bid']
+        try:
+            hl_ask = hl_tickers[ticker]['ask']
+            hl_bid = hl_tickers[ticker]['bid']
+            ll_ask = ll_tickers[ticker]['ask']
+            ll_bid = ll_tickers[ticker]['bid']
 
-        hl_spread = ((hl_ask - hl_bid) / hl_ask) * 100
-        ll_spread = ((ll_ask - ll_bid) / ll_ask) * 100
+            hl_spread = ((hl_ask - hl_bid) / hl_ask) * 100
+            ll_spread = ((ll_ask - ll_bid) / ll_ask) * 100
 
-        # Compare size of spread
+            # Compare size of spread
 
-        if ll_spread - hl_spread > spread_threshold:
-            hi_spread_tokens.append(ticker)
-            hl_volume = hl_tickers[ticker]['quoteVolume']
-            ll_volume = ll_tickers[ticker]['quoteVolume']
-            # print(ticker)
-            # print(f'Spread on {hl_exchange.name} = {hl_spread}')
-            # print(f'Quote volume on {hl_exchange.name} = {hl_volume}')
-            # print(f'Quote volume on {hl_exchange.name} = {ll_volume}')
-            # print(f'Spread on {ll_exchange.name} = {ll_spread}')
+            if ll_spread - hl_spread > spread_threshold:
+                hi_spread_tokens.append(ticker)
+                hl_volume = hl_tickers[ticker]['quoteVolume']
+                ll_volume = ll_tickers[ticker]['quoteVolume']
+                # print(ticker)
+                # print(f'Spread on {hl_exchange.name} = {hl_spread}')
+                # print(f'Quote volume on {hl_exchange.name} = {hl_volume}')
+                # print(f'Quote volume on {hl_exchange.name} = {ll_volume}')
+                # print(f'Spread on {ll_exchange.name} = {ll_spread}')
 
-            ticker_data = [now, ticker, f'{hl_exchange.name} / {ll_exchange.name}', hl_spread, ll_spread, (ll_spread - hl_spread), hl_volume, ll_volume]
-            data.append(ticker_data)
-
+                ticker_data = [now, ticker, f'{hl_exchange.name} / {ll_exchange.name}', hl_spread, ll_spread, (ll_spread - hl_spread), hl_volume, ll_volume]
+                data.append(ticker_data)
+        except TypeError:
+            print('Data likely missing')
+            continue
 
     # print(hi_spread_tokens)
     print(f'Found {len(hi_spread_tokens)} pairs with a spread differential of over {spread_threshold} on {ll_exchange.name}.')
